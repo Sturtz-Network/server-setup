76a76858dd7032bee160aade66ea1522ac2abce8
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-09-09 10:17:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 cd /
 mkdir NSCS
 cd /NSCS
+apt install -y mysql-server mysql-client
 curl -O http://software.virtualmin.com/gpl/scripts/install.sh
 sudo chmod +x install.sh
 sudo ./install.sh
+apt install tasksel php -y
+tasksel
 
+wall DONE!!!
