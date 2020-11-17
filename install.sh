2b1e3c3ae1e3047339315b57ec88fb21863f69f8
Nate Sturtz <sturtz110751@indianola.k12.ia.us>
2020-11-17 11:40:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
 sudo apt update
-sudo apt install webmin
+sudo apt install webmin 
 #bind
-apt install bind9
+apt install bind9 
 cd /
 mkdir NSCS
 cd /NSCS
 wget https://updates.duplicati.com/beta/duplicati_2.0.5.1-1_all.deb
 apt install -y ./duplicati_2.0.5.1-1_all.deb
-apt install -y mysql-server mysql-client apache2 php 
+apt install -y mysql-server mysql-client apache2 php python3 dnsutils php-cli libapache2-mod-php php-cgi php-mysql 
+a2enmod php7.4
 curl -O http://software.virtualmin.com/gpl/scripts/install.sh
 sudo chmod +x install.sh
 mysql <  nextcloud.sql
