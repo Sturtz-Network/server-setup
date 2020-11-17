50d6cc65d0fb20f1f1c43462ae3f650568cfc9a7
Nate Sturtz <sturtz110751@indianola.k12.ia.us>
2020-11-17 11:19:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
+#webmin
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
+sudo apt update
+sudo apt install webmin
+#bind
+apt install bind9
 cd /
 mkdir NSCS
 cd /NSCS
-apt install -y mysql-server mysql-client
+wget https://updates.duplicati.com/beta/duplicati_2.0.5.1-1_all.deb
+apt install -y ./duplicati_2.0.5.1-1_all.deb
+apt install -y mysql-server mysql-client apache2 php 
 curl -O http://software.virtualmin.com/gpl/scripts/install.sh
 sudo chmod +x install.sh
 sudo ./install.sh
-apt install tasksel php -y
-tasksel
 chown -R www-data:www-data /data
 wall DONE!!!
