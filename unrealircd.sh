2d4b383633f7641b045ed6db1c668c54ed92f722
Nate Sturtz <sturtz110751@indianola.k12.ia.us>
2020-12-29 13:41:05 -0600

    Update unrealircd.sh

--- unrealircd.sh
+++ unrealircd.sh
@@ -1,9 +1,5 @@
 #!/bin/bash/
-wget --trust-server-names https://www.unrealircd.org/downloads/unrealircd-latest.tar.gz
-tar -xvf unrealircd-5.0.X.tar.gz
-cd unrealircd-5.0.X
-./Config
-make
-make install
-cd ~/unrealircd
-cp conf/examples/example.conf conf/unrealircd.conf
+cd ~
+
+wget https://github.com/inspircd/inspircd/archive/v3.8.1.tar.gz
+tar -xvf v3.8.1.tar.gz
