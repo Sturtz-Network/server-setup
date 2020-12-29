65be4ade7ec8a78b70f2fd19992c9a08d6beaf3b
Nate Sturtz <sturtz110751@indianola.k12.ia.us>
2020-12-29 13:51:03 -0600

    Update unrealircd.sh

--- unrealircd.sh
+++ unrealircd.sh
@@ -1,10 +1,13 @@
 #!/bin/bash/
-cd ~
 
-wget https://github.com/inspircd/inspircd/archive/v3.8.1.tar.gz
-tar -xvf v3.8.1.tar.gz
-cd inspircd-3.8.1
-perl ./configure
+wget --trust-server-names https://www.unrealircd.org/downloads/unrealircd-latest.tar.gz
+tar -xvf 5.0.7.tar.gz
+cd unrealircd-5.0.7
+wall I.T. needed, URGENT I.T. NEEDED
+./Config
 make
 make install
+cd ~/unrealircd
+cp conf/examples/example.conf conf/unrealircd.conf
+
 wall IRC server installed NEED TO CONFIG https://ubuntu.com/tutorials/irc-server#6-server-configuration
