a83d42b206d7cb0d00ebb97b2e841eeca5b63fae
Nate Sturtz <sturtz110751@indianola.k12.ia.us>
2021-01-03 15:24:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@
 sudo apt-get install -y perl
 sudo apt-get install -y g++
 sudo apt-get install -y make
-##WALL I.T. NEEDED for installing unrealircd user
-adduser unrealircd
-##VIRTUALMIN  
-wall starting install of virtualmin!
-curl -O https://raw.githubusercontent.com/Sturtz-Network/server-setup/master/virtualmin.sh
 sudo chmod +x install.sh
 sudo ./install.sh
 #limnoria bot
@@ -28,8 +23,5 @@ cd /NSCS
 apt install -y mysql-server mysql-client apache2 php python3 dnsutils php-cli libapache2-mod-php php-cgi php-mysql
 apt install -y python apt-show-versions libapt-pkg-perl libauthen-pam-perl libio-pty-perl libnet-ssleay-perl curl
 a2enmod php7.4
-wget https://raw.githubusercontent.com/Sturtz-Network/server-setup/master/unrealircd.sh
-###UNREALIRCD###
-mv unrealircd.sh /home/nsturtz/unrealircd.sh
 
 wall DONE!!!
